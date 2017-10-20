e22d536030c52a7b3b337cbb310e4a2ad28eeee2
pratN <c3180089@uon.edu.au>
2017-10-20 13:39:17 +1100

    Did the things

--- build.py
+++ build.py
@@ -16,23 +16,33 @@ from create_targets import *
 from preprocessing import *
 from generate_subimages import *
 from generators import *
+import tensorflow as tf
+from tiramisu import *
+import keras
+# from keras_tqdm import tqdm_callback
 
-if __name__=="__main__":
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config = config)
 
+if __name__ == "__main__":
     images = np.load('images.npy')
     labels = np.load('labels.npy')
-
-    waldo_sub_imgs = np.load('waldo_sub_imgs.npy')
-    waldo_sub_labels = np.load('waldo_sub_labels.npy')
-
+    waldo_sub_imgs = np.load('wally_sub_imgs.npy')
+    waldo_sub_labels = np.load('wally_sub_labels.npy')
     gen_mix = seg_gen_mix(waldo_sub_imgs, waldo_sub_labels, images, labels)
-
     X, y = next(gen_mix)
+    freq0 = float(np.sum(labels == 0))
+    freq1 = float(np.sum(labels == 1))
+    weights = {0: 1 / freq0, 1: 1.}
+    sample_weights = np.zeros((X.shape[0], X.shape[1] * X.shape[2], 2))
+    sample_weights.shape
+    sample_weights[:, :, 0] = weights[0]
+    sample_weights[:, :, 1] = weights[1]
+    input_shape = (224, 224, 3)
 
-    print(X.shape)
-
-    '''
-    freq0 = float(np.sum(labels==0))
-    
-    freq1 = float(np.sum(labels==1))
-    '''
+    img_input = Input(shape=input_shape)
+    x = create_tiramisu(2, img_input, nb_layers_per_block=[4, 5, 7, 10, 12, 15], p=0.2, wd=1e-4)
+    model = Model(img_input, x)
+    model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.RMSprop(1e-3), metrics=["accuracy"], sample_weight_mode='temporal')
+    model.fit_generator(gen_mix,steps_per_epoch=5, epochs=1, verbose=0, class_weight=sample_weights)
