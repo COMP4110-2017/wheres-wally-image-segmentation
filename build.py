e8279871bc3f8ace1112fab0eb143d3f526ee51f
pratN <c3180089@uon.edu.au>
2017-10-21 02:31:48 +1100

    My computer bleeds for you animals

--- build.py
+++ build.py
@@ -25,7 +25,7 @@ from keras_tqdm import TQDMCallback
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
-sess = tf.Session(config = config)
+sess = tf.Session(config=config)
 
 if __name__ == "__main__":
     images = np.load('images.npy')
@@ -46,25 +46,70 @@ if __name__ == "__main__":
     img_input = Input(shape=input_shape)
     x = create_tiramisu(2, img_input, nb_layers_per_block=[4, 5, 7, 10, 12, 15], p=0.2, wd=1e-4)
     model = Model(img_input, x)
-    model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.RMSprop(1e-3), metrics=["accuracy"], sample_weight_mode='temporal')
-    a=datetime.datetime.now()
-    ep=1
-    model.fit_generator(gen_mix,steps_per_epoch=5, epochs=ep, verbose=0, callbacks=[TQDMCallback()], class_weight=sample_weights)
-    b=datetime.datetime.now()
-    print('Training Complete! Time: ', b-a)
+    model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.RMSprop(1e-3), metrics=["accuracy"],
+                  sample_weight_mode='temporal')
+    a = datetime.datetime.now()
+    ep = 1
+    model.fit_generator(gen_mix, steps_per_epoch=5, epochs=ep, verbose=0, callbacks=[TQDMCallback()],
+                        class_weight=sample_weights)
+    b = datetime.datetime.now()
+    print('Training Complete! Time: ', b - a)
+
+    # Save model
+    modelName = 'model_' + str(ep) + 'epochs.h5'
+    model.save(modelName)
+
+    # Display error history
+
+    # h = model.history.history
+    # plt.plot(h['loss'])
+    # plt.show()
+
+    # Predict on new image
+    gen_mix = seg_gen_mix(waldo_sub_imgs, waldo_sub_labels, images, labels)
+    X, y = next(gen_mix)
+    # plt.imshow(X[1] * std + mu)
+    # plt.show()
+    # pred = model.predict_on_batch(X)
+    # plt.imshow(pred[1].reshape(160, 160, 2)[:, :, 1])
+    # plt.show()
+
+    gen_mix = seg_gen_mix(waldo_sub_imgs, waldo_sub_labels, images, labels, tot_bs=6, prop=.67)
+
+    ep = 1
+    model.fit_generator(gen_mix, steps_per_epoch=3, epochs=ep, verbose=0, callbacks=[TQDMCallback()],
+                        class_weight=sample_weights)
+
+    # Save model
+    modelName = 'model_' + str(ep) + 'epochs.h5'
+    model.save(modelName)
+
+    # Display error history
+
+    # h = model.history.history
+    # plt.plot(h['loss'])
+    # plt.show()
+
+    model.fit_generator(gen_mix, steps_per_epoch=6, epochs=1, verbose=0, callbacks=[TQDMCallback()],
+                        class_weight=sample_weights)
+
+    ep = 5000
+    model.fit_generator(gen_mix, steps_per_epoch=6, epochs=ep, verbose=0, callbacks=[TQDMCallback()],
+                        class_weight=sample_weights)
+
+    modelName = 'model_' + str(ep) + 'epochs.h5'
+    model.save(modelName)
 
     h = model.history.history
     plt.plot(h['loss'])
     plt.show()
 
-    gen_mix = seg_gen_mix(waldo_sub_imgs, waldo_sub_labels, images, labels)
-    X, y = next(gen_mix)
     plt.imshow(X[1] * std + mu)
     plt.show()
     pred = model.predict_on_batch(X)
     plt.imshow(pred[1].reshape(160, 160, 2)[:, :, 1])
     plt.show()
-    modelName = 'model_' + str(ep) + 'epochs.h5'
-    model.save(modelName)
-
-
+    model.save('overnight.h5')
+    model.save('overnight_backup1.h5')
+    model.save('overnight_backup2.h5')
+    model.save('overnight_backup3.h5')
