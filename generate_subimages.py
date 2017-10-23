dc94195f692ca7524708917b1db436ec5b301765
pratN <c3180089@uon.edu.au>
2017-10-24 00:46:56 +1100

    Cleaned up some code

--- generate_subimages.py
+++ generate_subimages.py
@@ -1,13 +1,12 @@
 import numpy as np
 from params import *
-import matplotlib.pyplot as plt
 
 
 def extract_sub_image(img, box, side_length):
     xStart, xEnd, yStart, yEnd = box
-    i = img[xEnd-160:xStart+side_length, yEnd-side_length:yStart+side_length]
-    if i.shape[0]==0:
-        return img[xStart:xStart+(side_length+1), yEnd-side_length:yStart+side_length]
+    i = img[xEnd - 160:xStart + side_length, yEnd - side_length:yStart + side_length]
+    if i.shape[0] == 0:
+        return img[xStart:xStart + (side_length + 1), yEnd - side_length:yStart + side_length]
     else:
         return i
 
@@ -18,10 +17,11 @@ def find_box(label):
     """
     shape = label.shape
     yStart = np.argmax(label.sum(axis=0))
-    yEnd = int(yStart+np.max(np.unique(label.sum(axis=1))))
+    yEnd = int(yStart + np.max(np.unique(label.sum(axis=1))))
     xStart = np.argmax(label.sum(axis=1))
-    xEnd= int(xStart+np.max(np.unique(label.sum(axis=0))))
+    xEnd = int(xStart + np.max(np.unique(label.sum(axis=0))))
     return xStart, xEnd, yStart, yEnd
+
 
 def run():
     side_length = 160
@@ -36,7 +36,8 @@ def run():
     np.save(NUMPY_PATH + 'wally_sub_images.npy', np.array(wally_sub_images))
     np.save(NUMPY_PATH + 'wally_sub_labels.npy', np.array(wally_sub_labels))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     side_length = 160
     images = np.load(NUMPY_PATH + 'images.npy')
     labels = np.load(NUMPY_PATH + 'labels.npy')
@@ -46,21 +47,5 @@ if __name__=="__main__":
         box = find_box(label)
         wally_sub_images.append(extract_sub_image(images[i], box, side_length))
         wally_sub_labels.append(extract_sub_image(label, box, side_length))
-    np.save(NUMPY_PATH + 'wally_sub_images.npy',np.array(wally_sub_images))
-    np.save(NUMPY_PATH + 'wally_sub_labels.npy',np.array(wally_sub_labels))
-
-    '''
-    plt.imshow(wally_sub_images[0] * std + mu)
-    plt.show()
-    plt.imshow(wally_sub_labels[0])
-    plt.show()
-    plt.imshow(wally_sub_images[1] * std + mu)
-    plt.show()
-    plt.imshow(wally_sub_labels[1])
-    plt.show()
-    plt.imshow(wally_sub_images[2] * std + mu)
-    plt.show()
-    plt.imshow(wally_sub_labels[2])
-    plt.show()
-    '''
-
+    np.save(NUMPY_PATH + 'wally_sub_images.npy', np.array(wally_sub_images))
+    np.save(NUMPY_PATH + 'wally_sub_labels.npy', np.array(wally_sub_labels))
